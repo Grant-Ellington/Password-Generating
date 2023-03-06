056d6f361ad73b7802d034f0c3427dcbdd1d3d2b
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-06 16:04:19 -0600

    can set randomization to length

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment code here
+char = {
+  lowerCase: ['a','b','c','d',],
+  upperCase: ['A','B','C','D'],
+  numbers: [1,2,3,4],
+  special: ['!','@','#','$']
+}
 
+function generatePassword(){
 
+  var pwlength = prompt('How many characters is your password?')
+  password = []
+
+  for( let i=0; i<pwlength; i++){
+  var random = Math.floor(Math.random()*char.lowerCase.length)
+  password.push(char.lowerCase[random])
+  console.log(password.join(''))
+  }
+
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
